52d04df65a43de3841c8ddbb41ea208dac382b69
crazyCode2 <1178770858@qq.com>
2018-08-12 16:37:21 +0800

    完成首页数据请求

--- src/pages/MainPage/HomePage/index.js
+++ src/pages/MainPage/HomePage/index.js
@@ -5,24 +5,43 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   Text,
-  View,
+  ScrollView
 } from 'react-native';
+import ListRow from 'teaset/components/ListRow/ListRow';
+import { NewsStore } from '../../../store/NewsStore.js';
+import { toJS } from 'mobx';
+import { BaseContainer } from '../../../components';
+import { observer } from 'mobx-react';
 
+@observer
 export default class HomePage extends Component {
+
+  store = new NewsStore();
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>首页</Text>
-      </View>
-    )
+      <BaseContainer store={this.store} hideLeft title={'首页'} rightTitle={'详情'}>
+        <ScrollView style={styles.container} stickyHeaderIndices={[0]}>
+          <ListRow
+            title={'重新刷新'}
+            onPress={() => {
+              this.store.loadData();
+              console.log(toJS(this.store.data.tid));
+            }}
+          />
+          {
+            this.store.data.tid && this.store.data.tid.map((item, index) =>
+              <Text key={item.title}>{JSON.stringify(item)}</Text>
+            )
+          }
+        </ScrollView>
+      </BaseContainer>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+  },
 });
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,5 @@
  */
 // 添加组件的的自定义属性
 export {addCustomProps} from './addCustomProps';
+export {HttpUtil, ImageBody} from './HttpUtil';
+export {LoadingUtils} from './LoadingUtils';
