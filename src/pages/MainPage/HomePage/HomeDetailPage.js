af12914a43c69a5576acf56d39535a7d0bd351a1
crazyCode2 <1178770858@qq.com>
2018-08-26 16:33:40 +0800

    react-navigation  createDrawerNavigator

--- src/pages/MainPage/HomePage/HomeDetailPage.js
+++ src/pages/MainPage/HomePage/HomeDetailPage.js
@@ -8,7 +8,35 @@ import {
   View,
 } from 'react-native';
 import { BaseContainer } from '../../../components';
-// import { RouteHelper } from 'react-navigation-easy-helper';
+import Icon from 'react-native-vector-icons/Ionicons'; //引入图标
+import { createDrawerNavigator } from 'react-navigation';
+// 第一页
+import FirstScreen from './FirstScreen';
+// 第二页
+import SecondScreen from './SecondScreen';
+// 侧边菜单(抽屉)
+const MyDrawerNavigator = createDrawerNavigator({
+  First: {
+    screen: FirstScreen,
+  },
+  Second: {
+    screen: SecondScreen,
+  },
+},{
+  drawerWidth: 220, // 抽屉宽
+  drawerPosition: 'left', // 抽屉在左边还是右边
+  // contentComponent: CustomDrawerContentComponent,  // 自定义抽屉组件
+  contentOptions: {
+    initialRouteName: FirstScreen, // 默认页面组件
+    activeTintColor: '#008AC9',  // 选中文字颜色
+    activeBackgroundColor: '#f5f5f5', // 选中背景颜色
+    inactiveTintColor: '#000',  // 未选中文字颜色
+    inactiveBackgroundColor: '#fff', // 未选中背景颜色
+    style: {  // 样式
+      //
+    }
+  }
+});
 
 export default class HomeDetailPage extends Component {
   state = {
@@ -24,11 +52,18 @@ export default class HomeDetailPage extends Component {
     });
   }
 
+  // 显示或隐藏侧边菜单(抽屉)
+  toggleMenu(){
+    // this.props.navigation.openDrawer(); // 打开抽屉
+    // this.props.navigation.closeDrawer(); // 关闭抽屉
+  }
+
   render() {
     return (
       <BaseContainer
         leftPress={() => this.props.navigation.goBack()}
         title={'详情页'}
+        rightView={<Icon name='md-home' size={30} onPress={() => this.toggleMenu()} />}
       >
         <Text>{JSON.stringify(this.state.detail)}</Text>
       </BaseContainer>
